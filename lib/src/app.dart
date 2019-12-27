61af62667e54a4c988b4e49230bef6e71e5e75a1
Francois Venter <venter109@gmail.com>
2019-12-27 13:08:18 +0200

    http package

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class App extends StatefulWidget {
- createState() {
-   return _AppState();
- }
+  createState() {
+    return _AppState();
+  }
 }
 
 class _AppState extends State<App> {
   int counter = 0;
+
+  void fetchImage(){
+
+  }
 
   Widget build(context) {
     return MaterialApp(
@@ -16,16 +21,12 @@ class _AppState extends State<App> {
           title: Text("Let's see some images!"),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              counter++;
-              print(counter);
-            });
-          },
+          onPressed: fetchImage,
           child: Icon(Icons.add),
         ),
         body: Text('$counter'),
       ),
     );
   }
+
 }
---
+++ lib/src/models/image_model.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+class ImageModel {
+  int id;
+  String title, url;
+
+  ImageModel(this.id, this.title, this.url);
+  ImageModel.fromJson(inJson){
+    id = inJson.id;
+    title = inJson['title'];
+    url = inJson['url'];
+  }
+}
